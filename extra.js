e0c378b978778646728c664595024c51a0cf3029
alaynetheodore5 <astheod@aol.com>
2021-02-05 20:20:14 -0600

    totalDistance

--- extra.js
+++ extra.js
@@ -382,3 +382,17 @@ function matrixSearch(bigArr, smallArr) {
 }
 
 console.log(matrixSearch([[1,2,3],[4,5,6], [7,8,9]], [[2,3], [5,6]]));
+
+// Joe drives a taco truck in the booming town of Squaresburg. He uses an array of [x,y] coordinates corresponding to locations of his customers. They walk to his truck, but he is fair-minded so he wants to minimize total distance from truck to customers. City blocks are perfect squares, and every street is two-way, at perfect right angles. He only parks by street corners (coordinates like [37,-16]). Customers only travel on streets: coordinate [2,-2] is distance 4 from [0,0]. Joe checks the array before deciding where to park. Given a customer coordinate array, return an optimal taco truck location. Example: given [ [10,0], [-1,-10], [2,4] ], return [2,0], as total distance is 25 (8+13+4).
+
+function totalDistance(carr, testPoint) {
+  var total = 0;
+  // calculate total disstance each customer walks
+  for(var customer of carr) {
+      total += Math.abs(customer[0] - testPoint[0]);
+      total += Math.abs(customer[1] - testPoint[1]);
+  }
+  return total;
+}
+
+// console.log(totalDistance([ [10,0], [-1,-10], [2,4] ], [0,0] ));
