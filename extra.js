7997eb3c339aaa3109ec3c0fbcb1e8a6f7311d25
alaynetheodore5 <astheod@aol.com>
2021-02-09 21:53:20 -0600

    tacoTruck

--- extra.js
+++ extra.js
@@ -396,3 +396,50 @@ function totalDistance(carr, testPoint) {
 }
 
 // console.log(totalDistance([ [10,0], [-1,-10], [2,4] ], [0,0] ));
+
+function tacoTruck(carr){
+  var maxX = carr[0][0];
+  var minX = carr[0][0];
+  var maxY = carr[0][1];
+  var minY = carr[0][1];
+  for(var i=0; i<carr.length; i++) {
+      if(carr[i][0] > maxX){
+          maxX = carr[i][0];
+      }
+      if(carr[i][0] < minX) { 
+          minX = carr[i][0];
+      }
+      if(carr[i][1] > maxY) { 
+          maxY = carr[i][1];
+      }
+      if(carr[i][1] < minY) { 
+          minY = carr[i][1];
+      }
+      }
+      // console.log(maxX, minX, maxY, minY);
+      // var bestPos = {x: Infinty, y: Infinty};
+      // var bestTotal = Infinty;
+  
+      var bestPos = {x: minX, y: minY};
+      var bestTotal = totalDistance(carr, [minX, minY]);
+  
+      console.log(bestPos, bestTotal);
+  
+      for(var j=minX; j<maxX; j++){
+      for(var m=minY; m<maxY; m++){
+          // console.log([j, m]);
+          var tempDist = totalDistance(carr, [j, m]);
+          // console.log(tempDist, [j, m]);
+          if(tempDist < bestTotal) {
+          bestTotal = tempDist;
+          bestPos.x = j;
+          bestPos.y = m;
+          }
+      }
+  }
+  
+  return bestPos;
+
+}
+
+console.log(tacoTruck( [ [10,0], [-1,-10], [2,4] ] ))
