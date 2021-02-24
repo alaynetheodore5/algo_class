57b6e052ea3711bf67bc3e5755ce287f32153aaf
alaynetheodore5 <astheod@aol.com>
2021-02-23 21:31:40 -0600

    forloops

---
+++ forloops.js
@@ -0,0 +1,7 @@
+// here is my Array
+var myArray = ["APPLE", "BANANA", "PEAR", "MANGO"];
+
+// this is a for loop that will iterate through each item in myArray and print each item
+for(i=0; i<= myArray.length-1; i++){
+  console.log(myArray[i]);
+}
